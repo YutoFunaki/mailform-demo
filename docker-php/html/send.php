bc24e37f0c5e077d5fac06ef79dd6291b3acdadd
YutoFunaki <kaizuka0629@gmail.com>
2023-06-10 18:44:58 +0900

    messageだと送信できた

--- docker-php/html/send.php
+++ docker-php/html/send.php
@@ -1,9 +1,9 @@
 <?php
 try {
   //DB名、ユーザー名、パスワードを変数に格納
-  $dsn = 'mysql:dbname=mysql;host=mysql;charset=utf8';
-  $user = 'test';
-  $password = 'test';
+  $dsn = 'mysql:dbname=test;host=mysql;charset=utf8';
+  $user = 'root';
+  $password = 'root';
  
   $PDO = new PDO($dsn, $user, $password); //PDOでMySQLのデータベースに接続
   $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //PDOのエラーレポートを表示
@@ -11,17 +11,17 @@ try {
   //input.phpの値を取得
   $name = $_POST['name'];
   $mail = $_POST['mail'];
-  $comment = $_POST['comment'];
+  $message = $_POST['message'];
  
-  $sql = "INSERT INTO access (name, mail, comment) VALUES (:name, :mail, :comment)"; // テーブルに登録するINSERT INTO文を変数に格納　VALUESはプレースフォルダーで空の値を入れとく
+  $sql = "INSERT INTO access (name, mail, message) VALUES (:name, :mail, :message)"; // テーブルに登録するINSERT INTO文を変数に格納　VALUESはプレースフォルダーで空の値を入れとく
   $stmt = $PDO->prepare($sql); //値が空のままSQL文をセット
-  $params = array(':name' => $name, ':mail' => $mail, ':comment' => $comment); // 挿入する値を配列に格納
+  $params = array(':name' => $name, ':mail' => $mail, ':message' => $message); // 挿入する値を配列に格納
   $stmt->execute($params); //挿入する値が入った変数をexecuteにセットしてSQLを実行
  
   // 登録内容確認・メッセージ
   echo "<p>名前: " . $name . "</p>";
   echo "<p>メールアドレス: " . $mail . "</p>";
-  echo "<p>メッセージ: " . $comment . "</p>";
+  echo "<p>メッセージ: " . $message . "</p>";
   echo '<p>上記の内容をデータベースへ登録しました。</p>';
 } catch (PDOException $e) {
   exit('データベースに接続できませんでした。' . $e->getMessage());
